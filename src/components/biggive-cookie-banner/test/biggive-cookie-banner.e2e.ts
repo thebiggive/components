87b94ac6aef484b04322ac456ebcaf3c4d1a53b7
Barney Laurance <barney@thebiggive.org.uk>
2023-08-09 17:54:08 +0100

    DON-783: Update test code

--- src/components/biggive-cookie-banner/test/biggive-cookie-banner.e2e.ts
+++ src/components/biggive-cookie-banner/test/biggive-cookie-banner.e2e.ts
@@ -3,7 +3,7 @@ import { newE2EPage } from '@stencil/core/testing';
 describe('biggive-cookie-banner', () => {
   it('renders', async () => {
     const page = await newE2EPage();
-    await page.setContent('<biggive-cookie-banner></biggive-cookie-banner>');
+    await page.setContent('<biggive-cookie-banner blog-uri-prefix="http://localhost:30003"></biggive-cookie-banner>');
 
     const element = await page.find('biggive-cookie-banner');
     expect(element).toHaveClass('hydrated');
