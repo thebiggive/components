7ad3269485ccd2d3ccf0e01b43a3b61cd58cd47a
tomwpaterson-shape <tom@shapehistory.com>
2023-08-11 09:04:19 +0100

    Update biggive-image-card.spec.tsx

--- src/components/biggive-image-card/test/biggive-image-card.spec.tsx
+++ src/components/biggive-image-card/test/biggive-image-card.spec.tsx
@@ -1,10 +1,10 @@
 import { newSpecPage } from '@stencil/core/testing';
-import { BiggiveBasicCard } from '../biggive-image-card';
+import { BiggiveImageCard } from '../biggive-image-card';
 
 describe('biggive-image-card', () => {
   it('renders', async () => {
     const page = await newSpecPage({
-      components: [BiggiveBasicCard],
+      components: [BiggiveImageCard],
       html: `<biggive-image-card></biggive-image-card>`,
     });
     expect(page.root).toEqualHtml(`
@@ -14,19 +14,12 @@ describe('biggive-image-card', () => {
             <a>
              <div class="background-colour-white sleeve text-colour-black">
                 <div class="content-wrap">
-                  <div class="icon">
-                   <svg fill="none" height="39" viewBox="0 0 53 39" width="53" xmlns="http://www.w3.org/2000/svg">
-                     <path class="fill-black" d="M38.7009 13.6572L52.3535 38.6959H25.0386L38.7009 13.6572Z"></path>
-                     <path class="fill-primary" d="M20.4789 36.4199L6.36785e-06 5.89713e-05L40.9724 6.61352e-05L20.4789 36.4199Z"></path>
-                   </svg>
                  </div>
                  <div class="main-image-container">
                    <div class="image-wrap">
                      <img>
                    </div>
                 </div>
-                <h3 class="title"></h3>
-                 <div class="subtitle"></div>
                  <div class="teaser"></div>
                </div>
               </div>
