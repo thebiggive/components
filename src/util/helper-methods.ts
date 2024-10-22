a35d37d0ebf6a05ec77137ed2380e2d38aeebbe1
Barney Laurance <barney@thebiggive.org.uk>
2024-10-22 15:46:53 +0100

    COM-101: Make error message in console more descriptive

--- src/util/helper-methods.ts
+++ src/util/helper-methods.ts
@@ -1,6 +1,7 @@
 export const makeURL = (urlType: string, urlPrefix: string | undefined, relativeUrl: string) => {
   if (urlPrefix === undefined || urlPrefix.length === 0 || !urlPrefix.startsWith('http')) {
-    throw new Error(`${urlType} URL prefix must be set and start with http`);
+    const prefixType = typeof urlPrefix;
+    throw new Error(`${urlType} URL prefix must be set and start with http: bad prefix (${prefixType}) '${urlPrefix}'`);
   }
   return urlPrefix + '/' + relativeUrl;
 };
