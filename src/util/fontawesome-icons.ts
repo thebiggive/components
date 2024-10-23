c098f06761318bdf7e6e2ab6fe7e9a8b6fa661e7
Barney Laurance <barney@thebiggive.org.uk>
2024-10-23 17:12:45 +0100

    COM-101: Fix error logs about trailing commas in FontAwesome SVG paths

--- src/util/fontawesome-icons.ts
+++ src/util/fontawesome-icons.ts
@@ -240,27 +240,47 @@ export class FontAwesomeIconsService {
     const matchingItems = FontAwesomeIconsService.getBeneficiaries().filter(ii => ii.name === beneficiary);
 
     // For an unknown/invalid beneficiary, show 'other' symbol.
-    return matchingItems[0] ? matchingItems[0].icon : faPlus;
+    const icon = matchingItems[0] ? matchingItems[0].icon : faPlus;
+    removeTrailingComma(icon);
+    return icon;
   }
 
   static getMiscIcon(misc: string): IconDefinition {
     const matchingItems = FontAwesomeIconsService.getMisc().filter(ii => ii.name === misc);
 
     // For an unknown/invalid beneficiary, show 'other' symbol.
-    return matchingItems[0] ? matchingItems[0].icon : faPlus;
+    const icon = matchingItems[0] ? matchingItems[0].icon : faPlus;
+    removeTrailingComma(icon);
+    return icon;
   }
 
   static getSocialIcon(social: string): IconDefinition {
     const matchingItems = FontAwesomeIconsService.getSocials().filter(ii => ii.name === social);
 
     // For an unknown/invalid beneficiary, show 'other' symbol.
-    return matchingItems[0] ? matchingItems[0].icon : faPlus;
+    const icon = matchingItems[0] ? matchingItems[0].icon : faPlus;
+    removeTrailingComma(icon);
+    return icon;
   }
 
   static getCategoryIcon(category: string): IconDefinition {
     const matchingItems = FontAwesomeIconsService.getCategories().filter(ii => ii.name === category);
 
     // For an unknown/invalid category, show 'other' symbol.
-    return matchingItems[0] ? matchingItems[0].icon : faPlus;
+    const icon = matchingItems[0] ? matchingItems[0].icon : faPlus;
+    removeTrailingComma(icon);
+    return icon;
   }
+}
+
+/**
+ * Looks like the caretRight icon from FontAwesome (and maybe others) has an errant trailing comma in the path definition,
+ * which doesn't affect the appearance but leads to error noise in Chrome and Safari consoles, so removing the
+ * comma here.
+ * See https://stackoverflow.com/a/63798314/2526181
+ * @param iconDefinition
+ */
+function removeTrailingComma(iconDefinition: IconDefinition): void {
+  const path = iconDefinition.icon[4];
+  iconDefinition.icon[4] = path.toString().replace(/,\s*$/, '');
 }
