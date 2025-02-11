51a0a5be2b54a23d79954f9fde1cecb3409a51ca
Barney Laurance <barney@thebiggive.org.uk>
2025-02-11 17:45:08 +0000

    DON-1102: Fix bad references in components module declaration

--- angular/projects/components/src/lib/components.module.ts
+++ angular/projects/components/src/lib/components.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
-import { ComponentsComponent } from './components.component';
-
-
 
 @NgModule({
   declarations: [
-    ComponentsComponent
   ],
   imports: [
   ],
   exports: [
-    ComponentsComponent
   ]
 })
 export class ComponentsModule { }
