113ec4b9ccbde162622271989892364aa7492501
Barney Laurance <barney@thebiggive.org.uk>
2025-02-11 16:10:32 +0000

    DON-1102: Rebuild angular project as non-standalone with 19.1

--- angular/projects/components/src/public-api.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Public API Surface of components
- */
-
-// Directives
-export * from './lib/stencil-generated/components';
-
-// Package module
-export { ComponentsModule } from './lib/components.module';
