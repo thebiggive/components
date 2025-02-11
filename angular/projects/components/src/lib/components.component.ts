15af21965747960dedfd62f27880c9757fa23b44
Barney Laurance <barney@thebiggive.org.uk>
2025-02-11 16:16:16 +0000

    DON-1102: Switch angular build from application type to library type

---
+++ angular/projects/components/src/lib/components.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-components',
+  standalone: false,
+  template: `
+    <p>
+      components works!
+    </p>
+  `,
+  styles: ``
+})
+export class ComponentsComponent {
+
+}
