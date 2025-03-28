afc1b30e91fbc35af245aad4439ed5831257824a
Barney Laurance <barney@thebiggive.org.uk>
2025-03-28 12:46:15 +0000

    COM-102: Allow selecting quote icon colour in quote story

--- src/components/biggive-quote/biggive-quote.stories.tsx
+++ src/components/biggive-quote/biggive-quote.stories.tsx
@@ -11,6 +11,7 @@ export default {
       },
     },
     defaultTextColour: colorSelector,
+    quoteIconColour: colorSelector,
     quote: {
       name: 'Quote',
     },
@@ -28,6 +29,7 @@ const Template = (args: any) => `
               <biggive-quote
                 space-below="${args.spaceBelow}"
                 default-text-colour="${args.defaultTextColour}"
+                quote-icon-colour="${args.quoteIconColour}"
                 quote="${args.quote}"
                 attribution="${args.attribution}"
                 site-design="${args.siteDesign}"
@@ -42,6 +44,7 @@ export const QuoteComponent = {
   args: {
     spaceBelow: 4,
     defaultTextColour: 'black',
+    quoteIconColour: 'primary',
     quote:
       'Lorem ipsum dolor sit amet, has quod diam dolore cu, at solet interesset eam, cu nec affert quando legendos. Ut libris aperiam quo. Et tollit intellegebat mea. Dico nostrud vivendum no his, nam ne sumo illum iudicabit. Cu nec velit argumentum, no quando offendit neglegentur ius.  ',
     attribution: 'this is the attribution',
